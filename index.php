99599fb3ba6f27e5f531cd4ebdc28377ed20914e
Scout-Online <79670760+Scout-Online@users.noreply.github.com>
2021-02-26 14:17:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -18,18 +18,18 @@ $_SESSION['group'] = $group;
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="Streamlineicons-Streamline-Ux-Free-Ecology-globe.ico">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap2.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome2.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="animate.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="select2.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="util.css">
+	<link rel="stylesheet" type="text/css" href="main.css">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -38,7 +38,7 @@ $_SESSION['group'] = $group;
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt="" style="will-change: transform; transform: perspective(300px) rotateX(0deg) rotateY(0deg);">
-					<img src="images/img-01.png" alt="IMG">
+					<img src="img-01.png" alt="IMG">
 				</div>
 				<form method="POST" action="homescreen4.php">
 					
@@ -91,21 +91,21 @@ $_SESSION['group'] = $group;
 
 	
 <!--===============================================================================================-->	
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
 	<!--<script src="vendor/bootstrap/js/popper.js"></script>-->
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="bootstrap3.min.js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
+	<script src="select3.min.js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+	<script src="tilt.jquery.min.js"></script>
 	<script>
 		$(".js-tilt").tilt({
 			scale: 1.1
 		})
 	</script>
 <!--===============================================================================================-->
-	<script src="js/main.js"></script>
+	<script src="main.js"></script>
 
 
 
