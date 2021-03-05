fbc337ded13554a350153aa337fb48341153ad82
Scout-Online <79670760+Scout-Online@users.noreply.github.com>
2021-03-05 20:23:36 +0100

    Update and rename doorlink.html to doorlink.php

---
+++ doorlink.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php 
+	header("Location: https://play.google.com/store/apps/details?id=org.scoutonline.scoutonline&hl=nl&gl=US");
+	die();
+	?>
+</head>
+</html>
