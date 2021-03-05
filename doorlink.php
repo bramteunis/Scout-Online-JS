79f34d99cb9d0bd4a959fd25663265b224f9a972
Scout-Online <79670760+Scout-Online@users.noreply.github.com>
2021-03-05 20:33:26 +0100

    Update doorlink.php

--- doorlink.php
+++ doorlink.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<?php 
-	header("refresh:5; Location: https://play.google.com/store/apps/details?id=org.scoutonline.scoutonline&hl=nl&gl=US");
+	header("refresh:5; Location:https://play.google.com/store/apps/details?id=org.scoutonline.scoutonline&hl=nl&gl=US");
 	die();
 	?>
 </head>
