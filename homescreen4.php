3c93f63abe77338e18120c615145c4a90fbb5e54
Scout-Online <79670760+Scout-Online@users.noreply.github.com>
2021-03-25 15:33:22 +0100

    Update homescreen4.php

--- homescreen4.php
+++ homescreen4.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>The Town HTML CSS Template</title>
+    <title>Scout-Online | SW3LBP</title>
     <link rel="stylesheet" href="all.min.css" />
     <link rel="stylesheet" href="slick.css">
     <link rel="stylesheet" href="slick-theme.css">
