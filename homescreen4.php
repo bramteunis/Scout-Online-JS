fc5d1bd55bad5db8da5c86b2eff30e2a292a758e
Scout-Online <79670760+Scout-Online@users.noreply.github.com>
2022-01-21 14:06:44 +0100

    Update homescreen4.php

--- homescreen4.php
+++ homescreen4.php
@@ -24,6 +24,9 @@
 
     echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
   }
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 	  
 ini_set('session.cookie_lifetime', 60 * 60 * 24 * 7);
 ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 7);
