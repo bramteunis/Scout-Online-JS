255e95d88047b6ed2b39ac098d886cf0cf5b4ebc
Scout-Online <79670760+Scout-Online@users.noreply.github.com>
2022-01-21 14:05:12 +0100

    Update homescreen4.php

--- homescreen4.php
+++ homescreen4.php
@@ -26,6 +26,8 @@
   }
 	  
 ini_set('session.cookie_lifetime', 60 * 60 * 24 * 7);
+ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 7);
+ini_set('session.save_path', 'sessions');	  
 session_start();
 
 $group = $_SESSION['group'];
