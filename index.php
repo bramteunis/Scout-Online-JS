b7eccbbbef6d2931c0842134c930f5de3cd6856e
Scout-Online <79670760+Scout-Online@users.noreply.github.com>
2022-01-21 14:11:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ if(isset($_GET["group"]))
     	header("Location: selectgroup.php");
     }
 
+ini_set('session.cookie_lifetime', 60 * 60 * 24 * 7);
+ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 7);
+ini_set('session.save_path', 'sessions');	
 session_start();
 $_SESSION['group'] = $group;
 
