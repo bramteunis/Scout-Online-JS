00f57ea84aa6650a679800424dcacb3e07c39d31
Scout-Online <79670760+Scout-Online@users.noreply.github.com>
2022-01-23 14:06:52 +0100

    Update homescreen4.php

--- homescreen4.php
+++ homescreen4.php
@@ -1,3 +1,6 @@
+<?php 
+include 'header.php';
+?>
 <!DOCTYPE html>
 <html lang="nl">
   <head>
@@ -12,194 +15,14 @@
     <link rel="stylesheet" href="bootstrap4.min.css" />
     <link rel="stylesheet" href="templatemo-style.css" />
     <link rel="icon" type="image/png" href="favicon.ico"/>
-    <!--
-	The Town
-	https://templatemo.com/tm-525-the-town
-	-->
-  <?php 
-  function debug_to_console($data) {
-    $output = $data;
-    if (is_array($output))
-        $output = implode(',', $output);
-
-    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-  }
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-ini_set('session.cookie_lifetime', 60 * 60 * 24 * 7);
-	ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 7);
-	ini_set('session.save_path', 'sessions');	
-	session_start();
-//$group = $_SESSION['group'];
-	  
-if(!isset($_COOKIE["group_name"])) {
-    echo "Cookie named '" . "group_name" . "' is not set!";
-} else {
-    //echo "Cookie '" . "group_name" . "' is set!<br>";
-    $group = $_COOKIE["group_name"];
-}
-	  
-if($group == "beversochtend"){
-  $server = "eu-cdbr-west-03.cleardb.net";
-  $database = "heroku_b6ccf5347f986b7";
-  $username = "b73badccc28509";
-  $wachtwoord = "46e7ba9f";
-}
-if($group == "beversmiddag"){
-  $server = "eu-cdbr-west-03.cleardb.net";
-  $database = "heroku_f9c9ff2287354ae";
-  $username = "b8a6ec5dd4f831";
-  $wachtwoord = "26d0c1e2";
-}
-if($group == "sionihorde"){
-  $server = "eu-cdbr-west-03.cleardb.net";
-  $database = "heroku_6ddc7ca4fb6c487";
-  $username = "bd261e04c4f7ac";
-  $wachtwoord = "2646f655";
-}
-if($group == "mowglihorde"){
-  $server = "eu-cdbr-west-03.cleardb.net";
-  $database = "heroku_043cac3778e37ef";
-  $username = "b53a91493bfae6";
-  $wachtwoord = "b0c77f1c";
-}
-if($group == "shantihorde"){
-  $server = "eu-cdbr-west-03.cleardb.net";
-  $database = "heroku_3e38dc503c384ed";
-  $username = "bcf0fc9752adca";
-  $wachtwoord = "4433dfb8";
-}
-
-
-if($group == "gaaientroep"){
-  $server = "eu-cdbr-west-03.cleardb.net";
-  $database = "heroku_72bce5d900dc0c4";
-  $username = "b20d36b9e64e9a";
-  $wachtwoord = "592a9017";
-}
-if($group == "pmt"){
-  $server = "eu-cdbr-west-03.cleardb.net";
-  $database = "heroku_936cbdf524a261e";
-  $username = "bbb1086e05f46c";
-  $wachtwoord = "305a5102";
-}
-if($group == "ekerstroep"){
-  $server = "eu-cdbr-west-03.cleardb.net";
-  $database = "heroku_b6ccf5347f986b7";
-  $username = "b73badccc28509";
-  $wachtwoord = "46e7ba9f";
-}
-if($group == "ea757"){
-  $server = "eu-cdbr-west-03.cleardb.net";
-  $database = "heroku_1cb03cce5ef2216";
-  $username = "b9af828f2f4586";
-  $wachtwoord = "64cb70a1";
-}
-if($group == "ea595"){
-  $server = "eu-cdbr-west-03.cleardb.net";
-  $database = "heroku_4f770db4c2019e5";
-  $username = "bfbf8c292bd780";
-  $wachtwoord = "408b12a0";
-}
-
-
-if($group == "stam1"){
-  $server = "eu-cdbr-west-03.cleardb.net";
-  $database = "heroku_4e519908e65abd9";
-  $username = "b4e6692218c8bb";
-  $wachtwoord = "55c5369c";
-}
-if($group == "stam2"){
-  $server = "eu-cdbr-west-03.cleardb.net";
-  $database = "heroku_1112b001d39794d";
-  $username = "be0d90306e78fd";
-  $wachtwoord = "dd2610aa";
-}
-
-
-  if(!$connectie = mysqli_connect($server,$username,$wachtwoord,$database) ) {
-    $boodschap = "Verbinding is mislukt, omdat:".mysqli_connect_error($connectie).
-    "met foutnummer:".mysqli_connect_errno($connectie);
-    debug_to_console($boodschap);
-  } else{
-    $boodschap = "Verbinding is gelukt<br>";
-    debug_to_console($boodschap);
-  }
-
-  $getal1 = $_POST["email"];
-  $getal2 = $_POST["pass"];
-  $getal5 = str_replace('ë', 'e', $getal1);
-  $getal3 = strtolower($getal5);
-  $getal4 = ucfirst($getal5);
-	  
-  $a[] = $getal1;
-  $a[] = $getal2;
-  $_SESSION['email'] = $getal5;
-  $_SESSION['password'] = $getal2;
-  setcookie("email", $getal5, time() + (86400 * 30), "/");
-  setcookie("password", $getal2, time() + (86400 * 30), "/");
-debug_to_console($group);
-  $query = "SELECT * FROM {$group}";
-
-  if(!$resultaat = mysqli_query($connectie, $query) ) {
-    $boodschap .=  "query\" $query\" mislukt!"; 
-  } else{
-    $boodschap .= "query gelukt";
-  }
-  
-  
-  while($rij = mysqli_fetch_array($resultaat)){
-    #echo "Naam = ".$getal1." ww = ".$getal2;
-    if ($getal1 == $rij[1] or $getal3 == $rij[1] or $getal4 == $rij[1]){
-      if ($getal2 == $rij[2]) {
-        $gevonden = "Gevonden";
-        debug_to_console($gevonden);
-        break;
-      }else{
-        $gevonden = "Niet Gevonden";
-        debug_to_console($gevonden);
-      }
-    }else{
-      $gevonden = "Niet Gevonden";
-      debug_to_console($gevonden);
-    }
-  }
-
-  if ($gevonden != "Gevonden") {
-    ob_start(); // ensures anything dumped out will be caught
-
-    // do stuff here
-    $url = 'index.php'; 
-
-    // clear out the output buffer
-    while (ob_get_status()) 
-    {
-        ob_end_clean();
-    }
-
-    // no redirect
-   header( "Location: $url" );    
-  }
-  
-
-  mysqli_close($connectie);
-
-  #Schrijven naar database
-  
-  
-?>
   </head>
 <body>    
-    <!-- Hero section -->
     <section id="hero" class="text-white tm-font-big tm-parallax">
-      <!-- Navigation -->
       <nav class="navbar navbar-expand-md tm-navbar" id="tmNav">              
         <div class="container">   
           <div class="tm-next">
               <a href="#uitloggen" class="navbar-brand">Uitloggen</a>
           </div>             
-            
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
             <i class="fas fa-bars navbar-toggler-icon"></i>
           </button>
@@ -222,7 +45,6 @@ debug_to_console($group);
 
         </div>
       </nav>
-      
       <div class="text-center tm-hero-text-container">
         <div class="tm-hero-text-container-inner">
             <h2 class="tm-hero-title"><?php $getal5 = ucwords(strtolower($getal1)); echo "Welkom terug ".$getal5; ?>, bij Scout-online</h2>
@@ -363,10 +185,8 @@ debug_to_console($group);
         if(windowHeight > 830) {
           _offset = 210;
         }
-
         return _offset;
       }
-
       function setParallaxPosition($doc, multiplier, $object){
         var offset = getOffSet();
         var from_top = $doc.scrollTop(),
