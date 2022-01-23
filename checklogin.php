8f1cfd10dafa8c1ef2d1635542b42690f83e99ab
Scout-Online <79670760+Scout-Online@users.noreply.github.com>
2022-01-23 13:54:46 +0100

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -1,4 +1,12 @@
 <?php
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
